69f0a63ce8e41c1829d9630d7c565dc84421ee95
baharjema <bdjema_ys@yahoo.co.id>
2024-03-07 22:37:38 +0700

    testing fastapi

--- app.py
+++ app.py
@@ -2,4 +2,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Shopify Shipping Ina')
+st.title('Shopify Shipping Indonesia')
+
+if not hasattr(st, 'already_started_server'):
+    st.already_started_server = True
+
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
+
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
+
+    from fastapi import FastAPI
+    app = FastAPI()
+
+    @app.post('/shipping-rate')
+    def shipping_rate():
+        return {"item": "Eco bottle"}
+    pass
+
